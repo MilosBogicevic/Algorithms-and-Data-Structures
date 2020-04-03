d07bebc0c2862c5112eb16c0cb47bc81961d3813
unknown <milbog.development@gmail.com>
2020-04-03 22:17:41 +0200

    Smallest number in an array.

---
+++ smallestNumber.js
@@ -0,0 +1,17 @@
+let array = [285, 82, 52, 68, 112, 542, 5, 1280, 54, 65, 98, 1540];
+let smallest = array[0];
+
+for (i = 0; i < array.length; i++) {
+  if (array[i] < smallest) {
+    smallest = array[i];
+  }
+}
+
+console.log(smallest);
+
+/*  
+  To run this file:
+  1. Install code editor (Visual Studio Code) and Node.js
+  2. Open file with code editor
+  3. Type in terminal: node smallestNumber.js 
+*/
