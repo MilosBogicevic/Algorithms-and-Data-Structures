64b45942659612a1a76ad620085eea736737c126
unknown <milbog.development@gmail.com>
2020-04-06 16:57:15 +0200

    Factorial of a number

---
+++ factorial.js
@@ -0,0 +1,12 @@
+function factorial(number) {
+  if (number == 0 || number == 1) {
+    return 1;
+  }
+
+  for (let i = number - 1; i >= 1; i--) {
+    number *= i;
+  }
+  return number;
+}
+
+console.log(factorial(5));
