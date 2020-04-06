63444c904c00c4378645a75ea7d9638cf0fd39fd
unknown <milbog.development@gmail.com>
2020-04-06 17:09:51 +0200

    Factorial with recursion.

--- factorial.js
+++ factorial.js
@@ -10,3 +10,22 @@ function factorial(number) {
 }
 
 console.log(factorial(5));
+
+
+// Factorial with recursion
+function factorial2(number) {
+  if (number == 0 || number == 1) {
+    return 1;
+  }
+  // Recursion
+  return factorial2(number - 1) * number;
+}
+
+console.log(factorial2(5));
+
+/*  
+  To run this file:
+  1. Install code editor (Visual Studio Code) and Node.js
+  2. Open file with code editor
+  3. Type in terminal: node factorial.js 
+*/
