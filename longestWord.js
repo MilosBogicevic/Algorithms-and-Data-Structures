c5a2cf971798a2870b8791a80925abd72ddf80f9
unknown <milbog.development@gmail.com>
2020-04-10 21:14:02 +0200

    Longest word in array

--- longestWord.js
+++ longestWord.js
@@ -1,2 +1,15 @@
 // Longest word in array
-function longestWord(words) {}
+function longestWord(words) {
+  // Declaring a variable "longest" with the first word in the array
+  let longest = words[0];
+  for (let i = 1; i < words.length; i++) {
+    // If the new word in the array is longer than the longest, replace it
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+  return longest;
+}
+
+let words = ["I", "love", "programming", "Do", "you", "love", "it"];
+console.log(longestWord(words));
