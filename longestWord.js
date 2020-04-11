3ad5da4fa316b0f7f23d9af87cfab590335ea2bc
unknown <milbog.development@gmail.com>
2020-04-11 20:47:06 +0200

    Find longest word with reduce method.

--- longestWord.js
+++ longestWord.js
@@ -17,3 +17,14 @@ console.log(longestWord(words));
 // Find longest word with sort method
 let words2 = ["hello", "hi", "sort", "method"];
 console.log(words2.sort((a, b) => b.length - a.length)[0]);
+
+// Find longest word with reduce method
+let words3 = ["apple", "banana", "grapefruit"];
+console.log(words3.reduce((a, b) => (b.length > a.length ? b : a)));
+
+/*  
+  To run this file:
+  1. Install code editor (Visual Studio Code) and Node.js
+  2. Open file with code editor
+  3. Type in terminal: node longestWord.js 
+*/
