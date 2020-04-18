5110288b03389db9d0aba319ef493ad490ddb47c
MilosBogicevic <milbog.development@gmail.com>
2020-04-18 23:21:51 +0200

    Print a reversed array

---
+++ Java/src/ReverseAnArray.java
@@ -0,0 +1,18 @@
+public class ReverseAnArray {
+    public static void main(String[] args) {
+//      Initialize an array
+        int [] numbers = new int [] {25,45, 850, 221, 5, 15, 1280};
+
+//      Print an original array
+        System.out.println("Original array: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+//      Print a reversed array
+        System.out.println("\nReversed array: ");
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
