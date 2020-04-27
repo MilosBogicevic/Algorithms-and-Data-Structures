9bfe42126aee8c61429e2021706d8a7c933d3d89
MilosBogicevic <milbog.development@gmail.com>
2020-04-27 19:03:10 +0200

    Sort an array ASC

--- Java/src/SortElements.java
+++ Java/src/SortElements.java
@@ -10,5 +10,23 @@ public class SortElements {
         for (int i = 0; i < numbers.length; i++){
             System.out.print(numbers[i] + " ");
         }
+
+//      Sort an array ASC
+        for (int i = 0; i < numbers.length; i++){
+            for (int j = i + 1; j < numbers.length; j++){
+                if (numbers[i] > numbers[j]){
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        System.out.println();
+
+//      Print an array ASC
+        System.out.println("Ascending order: ");
+        for (int i = 0; i < numbers.length; i++){
+            System.out.print(numbers[i] + " ");
+        }
     }
 }
