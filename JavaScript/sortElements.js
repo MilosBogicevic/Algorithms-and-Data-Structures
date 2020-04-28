26777977fec2fac2e20c13dfdd3999a5b54f9376
MilosBogicevic <milbog.development@gmail.com>
2020-04-28 21:51:43 +0200

    Sort an array (ASCENDING)

--- JavaScript/sortElements.js
+++ JavaScript/sortElements.js
@@ -1,6 +1,18 @@
 function sortElements(numbers) {
   temp = 0;
   console.log("Original array: " + numbers);
+
+  // Sort an array (ASCENDING)
+  for (i = 0; i < numbers.length; i++) {
+    for (j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] > numbers[j]) {
+        temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+      }
+    }
+  }
+  console.log("Sorted array ASC: " + numbers);
 }
 
 const numbers = [15, 8, 2, 6, 5, 12, 3, 18, 16, 25, 22, 85, 43, 56];
