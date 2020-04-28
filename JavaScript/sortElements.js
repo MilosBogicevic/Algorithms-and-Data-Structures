ba568ea655d0ba69f10ad677f45dc054a65b9fa6
MilosBogicevic <milbog.development@gmail.com>
2020-04-28 21:56:22 +0200

    Sort an array (DESCENDING)

--- JavaScript/sortElements.js
+++ JavaScript/sortElements.js
@@ -12,7 +12,19 @@ function sortElements(numbers) {
       }
     }
   }
-  console.log("Sorted array ASC: " + numbers);
+  console.log("Sorted array (ASCENDING): " + numbers);
+
+  // Sort an array (DESCENDING)
+  for (i = 0; i < numbers.length; i++) {
+    for (j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] < numbers[j]) {
+        temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+      }
+    }
+  }
+  console.log("Sorted array (DESCENDING): " + numbers);
 }
 
 const numbers = [15, 8, 2, 6, 5, 12, 3, 18, 16, 25, 22, 85, 43, 56];
