45609f98f021fcb14d3b0ad1384be70bc445803d
MilosBogicevic <milbog.development@gmail.com>
2020-04-28 21:45:36 +0200

    Sort Elements - Original array.

---
+++ JavaScript/sortElements.js
@@ -0,0 +1,7 @@
+function sortElements(numbers) {
+  temp = 0;
+  console.log("Original array: " + numbers);
+}
+
+const numbers = [15, 8, 2, 6, 5, 12, 3, 18, 16, 25, 22, 85, 43, 56];
+sortElements(numbers);
