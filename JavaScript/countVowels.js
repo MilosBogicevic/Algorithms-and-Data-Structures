aa3a4c63ce2af09affa1aaa1e6ddbda8f016ab19
MilosBogicevic <milbog.development@gmail.com>
2020-04-29 21:36:35 +0200

    Counting the vowels in a string.

--- JavaScript/countVowels.js
+++ JavaScript/countVowels.js
@@ -3,13 +3,7 @@ function countNumberOfVowels(input) {
   let words = input.toLowerCase();
 
   for (i = 0; i < words.length; i++) {
-    if (
-      words[i] == "a" ||
-      words[i] == "e" ||
-      words[i] == "i" ||
-      words[i] == "o" ||
-      words[i] == "u"
-    ) {
+    if (words[i] == "a" || words[i] == "e" || words[i] == "i" || words[i] == "o" || words[i] == "u") {
       count++;
     }
   }
